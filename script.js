59baff70da98b4e9a2676d3954f2a5e509c554ac
Gabrielfonseca1008 <gfcruz06@gmail.com>
2026-02-15 11:44:13 -0300

    Código finalizado

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("olá, mundo!")
+class hero{
+    constructor(name, age, type){
+        this.name = name;
+        this.age = age;
+        this.type = type;
+    }
+    attack(){
+        let attackType;
+        switch(this.type){
+            case "mago":
+                attackType = "magia";
+                break;
+            
+            case "guerreiro":
+                attackType = "espada";
+                break;
+            
+            case "monge":
+                attackType = "artes marciais";
+                break;
+
+            case "ninja":
+                attackType = "shuriken";
+                break;
+            
+            default:
+                attackType = "Classe desconhecida";
+        }
+    console.log(`${this.name} ataca com ${attackType}`);
+    }
+}
+
+let hero1 = new hero("Kratos", 250, "guerreiro");
+
+console.log(hero1);
+hero1.attack(); 
